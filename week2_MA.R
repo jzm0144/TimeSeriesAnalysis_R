c7f5abf1c1944bfe2b1f5acc979e1fa21f17ecd7
jzm0144 <jzm0144@auburn.edu>
2024-06-21 15:57:06 -0500

    Week2 MA Process and their ACF Analysis

--- week2_MA.R
+++ week2_MA.R
@@ -1,4 +1,7 @@
+# Remove all variables from the RStudio Memory
 rm(list = ls())
+# Remove all visuals from the RStudio Memory
+try(dev.off())
 N = 10000
 
 # Generate Noise
@@ -6,15 +9,50 @@ noise = rnorm(N)
 
 # Introduce a Variable
 ma2 = NULL
+ma3 = NULL
+ma4 = NULL
+ma5 = NULL
 
 # Generate an MA2 Process
 for(i in 3:N){
   ma2[i] = noise[i]+0.5*noise[i-1]+0.5*noise[i-2]
 }
 
+# Generate an MA3 Process
+for(i in 4:N){
+  ma3[i] = noise[i]+0.5*noise[i-1]+0.5*noise[i-2]+0.2*noise[i-3]
+}
+
+# Generate an MA4 Process
+for(i in 5:N){
+  ma4[i] = noise[i]+0.5*noise[i-1]+0.5*noise[i-2]+0.2*noise[i-3]+0.1*noise[i-4]
+}
+
+# Generate an MA5 Process
+for(i in 6:N){
+  ma5[i] = noise[i]+0.5*noise[i-1]+0.5*noise[i-2]+0.3*noise[i-3]+0.2*noise[i-4]+0.1*noise[i-5]
+}
+
+
+
+
+par(mfrow=c(4,2))
 # Cut the first few readings
-moving_average_process = ma2[3:N]
-par(mfrow = c(2, 1))
-plot(ts(moving_average_process), main='MA2 TimeSeries', xlab='time', ylab='value',col='red')
-acf(ts(moving_average_process), type='correlation', main='ACF (Before)', col='red')
+MA2 = ma2[3:N]
+MA3 = ma3[4:N]
+MA4 = ma4[5:N]
+MA5 = ma5[6:N]
+
+plot(ts(MA2), main='MA2 TimeSeries', xlab='time', ylab='value',col='red')
+acf(ts(MA2), type='correlation', main='ACF (Corr)', col='red')
+
+plot(ts(MA3), main='MA3 TimeSeries', xlab='time', ylab='value',col='red')
+acf(ts(MA3), type='correlation', main='ACF (Corr)', col='red')
+
+plot(ts(MA4), main='MA4 TimeSeries', xlab='time', ylab='value',col='red')
+acf(ts(MA4), type='correlation', main='ACF (Corr)', col='red')
+
+plot(ts(MA5), main='MA5 TimeSeries', xlab='time', ylab='value',col='red')
+acf(ts(MA5), type='correlation', main='ACF (Corr)', col='red')
+title("Spikes of ACF show Order of MA process", line = -1, outer = TRUE)
 
--- week2_acf_function.R
+++ week2_acf_function.R
@@ -1,4 +1,25 @@
-purely_rand_process = ts(rnorm(100))
-print(purely_rand_process)
-hist(purely_rand_process)
-(acf(purely_rand_process, type='covariance'))
+# Remove all variables from the RStudio Memory
+rm(list = ls())
+# Remove all visuals from the RStudio Memory
+try(dev.off())
+
+y = NULL
+y[1] = 0
+
+# Create a Sine wave Dataset
+for(i in 2:1000){
+  y[i] = sin(2*3.14*0.02*i)
+}
+
+
+purely_rand_process = ts(rnorm(1000))
+sine_process = ts(y)
+par(mfrow=c(2,3))
+plot(sine_process, main='Sine Process')
+hist(sine_process, main='Histogram of Sine Process')
+acf(sine_process, type='covariance', main='ACF')
+
+
+plot(purely_rand_process, main='Purely Random Process')
+hist(purely_rand_process, main='Histogram of Random Process')
+acf(purely_rand_process, type='covariance', main='ACF')
