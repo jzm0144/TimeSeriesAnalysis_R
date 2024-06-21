ae90300f2781b75c3d064690e99b4061412fb1b0
jzm0144 <jzm0144@auburn.edu>
2024-06-21 12:43:56 -0500

    week2_lesson1 updated

--- week2_lesson1.R
+++ week2_lesson1.R
@@ -1,17 +1,24 @@
+# Remove all variables from the RStudio Memory
 rm(list = ls())
+# Remove all visuals from the RStudio Memory
+try(dev.off())
+
 require(astsa)
 require(graphics)
+require(datasets)
 
+par(mfrow=c(3,1))
 # Johnson Johnson Data
-#data = jj
-#print(summary(jj))
-#plot(jj, main='Johnson Johnson Earning Data (Quarterly)', xlab='Years', type='o', col='red')
+data = jj
+print(summary(jj))
+plot(jj, main='Johnson Johnson Earning Data (Quarterly)', xlab='Years', type='o', col='red')
 
 
 # Neumonia and Flu Datset
-#data = flu
-#plot(data, main='Neuomnia and Flu Deaths/10000 (Monthly)', xlab='Months', type='o', col='red')
+data = flu
+plot(data, main='Neuomnia and Flu Deaths/10000 (Monthly)', xlab='Months', type='o', col='red')
+
 
 # Land-ocean Temperature Deviations Dataset
-#data = ts(scan('/globtemp.dat'))
-#plot(data, main='Land-ocean Temperature Deviations (Quarterly)', xlab='Years', type='o', col='red')
+data = ts(scan('globtemp.dat'))
+plot(data, main='Land-ocean Temperature Deviations (Quarterly)', xlab='Years', type='o', col='red')
