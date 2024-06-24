cb4da7e09c5b6079404b86995977b8ec628a9cac
jzm0144 <jzm0144@auburn.edu>
2024-06-24 11:16:26 -0500

    Week4 - ACF and PACF of AR processes

--- week4_AR_acf_pacf.R
+++ week4_AR_acf_pacf.R
@@ -1,15 +1,35 @@
-#phi1 = 0.2; phi2 = 0.3; phi3=0.1; phi3.5= 0.01; phi4 = -0.1; phi5 = -0.3; phi6=-0.2;
-#par(mfrow=c(3,1))
-#data.ts = arima.sim(n = 1500, list(ar = c(phi1, phi2, phi3,phi3.5, phi4, phi5, phi6)))
-#plot(data.ts, 
-#     main = 
-#       paste("Autoregressive Process with phi1=", phi.1," phi2=",phi.2 ) ) 
-#acf(data.ts, type='correlation', main='ACF Plot')
-#acf(data.ts, type='partial', main="PACF Plot")
+# Remove all variables from the RStudio Memory
+rm(list = ls())
+# Remove all visuals from the RStudio Memory
+try(dev.off())
 
-phi1 = 1.3315874; phi2 = -0.4445447;
-par(mfrow=c(3,1))
-data.ts = arima.sim(n = 1500, list(ar = c(phi1, phi2)))
-plot(data.ts, main = paste("Autoregressive Process with phi1=", phi1," phi2=",phi2 ) ) 
-acf(data.ts, type='correlation', main='ACF Plot')
-acf(data.ts, type='partial', main="PACF Plot")
+# This script allows you to examine the ACF and PACF plots of AR Processes.
+# The spikes of PACF plot indicate the order of the AR process
+
+
+par(mfrow=c(4,3), oma=c(0,1,1,1))
+phi1 = 1.34; phi2 = -0.44;
+data = arima.sim(n = 1500, list(ar = c(phi1, phi2)))
+plot(data, main = paste("AR2 Process (phi1=", phi1," phi2=",phi2,")" ), col='red') 
+acf(data, type='correlation', main='', col='red')
+acf(data, type='partial',main='', col='red')
+
+phi1 = 1.34; phi2 = -0.64; phi3=0.2
+data = arima.sim(n = 1500, list(ar = c(phi1, phi2, phi3)))
+plot(data, main = paste("AR3 Process (",phi1, phi2, phi3,')'), col='red') 
+acf(data, type='correlation', main='', col='red')
+acf(data, type='partial',main='', col='red')
+
+phi1 = 1.34; phi2 = -0.54; phi3=0.1; phi4=-0.22
+data = arima.sim(n = 1500, list(ar = c(phi1, phi2, phi3, phi4)))
+plot(data, main = paste("AR4 Process (",phi1, phi2, phi3, phi4,')'), col='red') 
+acf(data, type='correlation', main='', col='red')
+acf(data, type='partial',main='', col='red')
+
+phi1 = 1.34; phi2 = -0.54; phi3=0.1; phi4=-0.32; phi5=0.15
+data = arima.sim(n = 1500, list(ar = c(phi1, phi2, phi3, phi4, phi5)))
+plot(data, main = paste("AR5 Process (",phi1, phi2, phi3, phi4, phi5,')'), col='red') 
+acf(data, type='correlation', main='', col='red')
+acf(data, type='partial',main='', col='red')
+
+title("ACF & PACF of AR Processes", line = -1, outer = TRUE, font.main=2, cex.main=1.7)
